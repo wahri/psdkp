dfe39346c383c5e442367c45f20e716be0af856b
nurhanifahhh <49105692+sukristiyo@users.noreply.github.com>
2022-03-21 11:23:42 +0700

    tampilan depan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,12 +13,11 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="../../index2.html"><b>Admin</b>LTE</a>
+            <a href="#"><b>PSDKP</b></a>
         </div>
         <!-- /.login-logo -->
         <div class="card">
             <div class="card-body login-card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
                 @error('email')
                     <div class="alert alert-danger" role="alert">
                         {{ $message }}
@@ -31,7 +30,7 @@
                             placeholder="Username" autofocus>
                         <div class="input-group-append">
                             <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
+                                <span class="fas fa-user"></span>
                             </div>
                         </div>
                     </div>
@@ -44,27 +43,13 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember">
-                                <label for="remember">
-                                    Remember Me
-                                </label>
-                            </div>
-                        </div>
                         <!-- /.col -->
-                        <div class="col-4">
+                        <div class="col-12">
                             <button type="submit" class="btn btn-primary btn-block">Sign In</button>
                         </div>
                         <!-- /.col -->
                     </div>
                 </form>
-
-                <!-- /.social-auth-links -->
-
-                <p class="mb-1">
-                    <a href="forgot-password.html">I forgot my password</a>
-                </p>
                 @if (Route::has('register'))
                     <p class="mb-0">
                         <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
